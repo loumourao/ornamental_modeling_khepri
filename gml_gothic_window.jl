2bb13c3964f798303b9dc6b02f9e7e2890e4f12f
loumourao <71392356+loumourao@users.noreply.github.com>
2024-05-12 01:44:04 +0100

    Added sub archs and some more

--- gml_gothic_window.jl
+++ gml_gothic_window.jl
@@ -3,7 +3,7 @@ backend(autocad)
 
 delete_all_shapes()
 
-function gothic_window(center, width, height, arch_excess)
+function gothic_window(center, width, height, arch_excess, recursion_level, distance_to_sub_arch)
     #=
     center - the normal of a given window
     width - the width of the main arch
@@ -24,15 +24,34 @@ function gothic_window(center, width, height, arch_excess)
         right_circle = surface(circle(right_circle_midpoint, circle_radius))
         arch = intersection(left_circle, right_circle)
 
-        # main arch calculations
+        # arch
         union(body, arch)
     end
 
-    function sub_arch(center, width, height, arch_excess)
-
+    function get_displaced_sub_archs_centers(new_width, distance_to_parent_arch)
+        displaced_center_x = new_width/2
+        displaced_center_y = -distance_to_parent_arch/2
+        left_sub_arch_center = xy(-displaced_center_x, displaced_center_y)
+        right_sub_arch_center = xy(displaced_center_x, displaced_center_y)
+        return (left_sub_arch_center, right_sub_arch_center)
     end
 
-    main_arch(center, width, height, arch_excess)
+    main = main_arch(center, width, height, arch_excess)
+
+    previous_width = width
+    previous_height = height
+
+    for level = 1:recursion_level
+        current_sub_height = previous_height - distance_to_sub_arch[level]
+        current_sub_width = previous_width/2
+        new_displaced_centers = get_displaced_sub_archs_centers(current_sub_width, distance_to_sub_arch[level])
+    
+        left_sub = main_arch(new_displaced_centers[1], current_sub_width, current_sub_height, arch_excess)
+        right_sub = main_arch(new_displaced_centers[2], current_sub_width, current_sub_height, arch_excess)
+
+        previous_width = current_sub_width
+        previous_height = current_sub_height
+    end
 end
 
-gothic_window(xy(0,0), 10, 10, 1)
+gothic_window(xy(0,0), 10, 10, 1, 2, (2, 2))
