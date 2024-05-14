5d5da3edd10f17c0326a44c392119fbf1a276d07
loumourao <71392356+loumourao@users.noreply.github.com>
2024-05-14 01:01:54 +0100

    Fixed ill programmed recursion

--- gml_gothic_window.jl
+++ gml_gothic_window.jl
@@ -28,30 +28,25 @@ function gothic_window(center, width, height, arch_excess, recursion_level, dist
         union(body, arch)
     end
 
-    function get_displaced_sub_archs_centers(new_width, distance_to_parent_arch)
+    function get_displaced_sub_archs_centers(previous_center, new_width, distance_to_parent_arch)
         displaced_center_x = new_width/2
         displaced_center_y = -distance_to_parent_arch/2
-        left_sub_arch_center = xy(-displaced_center_x, displaced_center_y)
-        right_sub_arch_center = xy(displaced_center_x, displaced_center_y)
+        left_sub_arch_center = previous_center + vxy(-displaced_center_x, displaced_center_y)
+        right_sub_arch_center = previous_center + vxy(displaced_center_x, displaced_center_y)
         return (left_sub_arch_center, right_sub_arch_center)
     end
 
     main = main_arch(center, width, height, arch_excess)
 
-    previous_width = width
-    previous_height = height
+    if recursion_level > 0
+        print(distance_to_sub_arch)
+        sub_arch_width = width/2
+        sub_arch_height = height - distance_to_sub_arch[1]
+        sub_archs_centers = get_displaced_sub_archs_centers(center, sub_arch_width, distance_to_sub_arch[1])
 
-    for level = 1:recursion_level
-        current_sub_height = previous_height - distance_to_sub_arch[level]
-        current_sub_width = previous_width/2
-        new_displaced_centers = get_displaced_sub_archs_centers(current_sub_width, distance_to_sub_arch[level])
-    
-        left_sub = main_arch(new_displaced_centers[1], current_sub_width, current_sub_height, arch_excess)
-        right_sub = main_arch(new_displaced_centers[2], current_sub_width, current_sub_height, arch_excess)
-
-        previous_width = current_sub_width
-        previous_height = current_sub_height
+        left_sub_arch = gothic_window(sub_archs_centers[1], sub_arch_width, sub_arch_height, arch_excess, recursion_level - 1, distance_to_sub_arch[2:end])
+        right_sub_arch = gothic_window(sub_archs_centers[2], sub_arch_width, sub_arch_height, arch_excess, recursion_level - 1, distance_to_sub_arch[2:end])
     end
 end
 
-gothic_window(xy(0,0), 10, 10, 1, 2, (2, 2))
+gothic_window(xy(0,0), 10, 10, 1, 2, [3, 3])
